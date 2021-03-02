f2faca0ab8cec890e4eab08f5b95a783be49b6d9
gingersnaps <37437483+dbdtrapper@users.noreply.github.com>
2021-03-03 05:31:04 +0900

    Update 백준_2294_동전2.java

--- 백준_2294_동전2.java
+++ 백준_2294_동전2.java
@@ -1,31 +1,27 @@
 import java.util.Scanner;
 
 public class 백준_2294_동전2 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int N=sc.nextInt();
-		int K=sc.nextInt();
-		int []coin = new int[N+1];
-		int []dp = new int[K+1];
-		for (int i = 0; i <= K; i++) {
-			dp[i]=Integer.MAX_VALUE;
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		int[] coin = new int[N];
+		int[] dp = new int[K + 1];
+		for (int n = 0; n < N; n++) {
+			coin[n] = sc.nextInt();
 		}
-		for (int n = 1; n <= N; n++) {
-			coin[n]=sc.nextInt();
-			dp[coin[n]]=1;
+		for (int i = 1; i < dp.length; i++) {
+			dp[i] = 100001;
 		}
-		dp[0]=0;
-		dp[1]=1;
-		for (int c = 1; c <= N; c++) {
-			for (int d = coin[c]; d <= K; d++) {//가장 작은 동전부터
-				dp[d]=Math.min(dp[d], dp[d-coin[c]]+1);
+
+		for (int c = 0; c < coin.length; c++) {
+			for (int d = coin[c]; d <= K; d++) {// 가장 작은 동전부터
+				dp[d] = Math.min(dp[d], dp[d - coin[c]] + 1);
 			}
 		}
-		if(dp[K]==Integer.MAX_VALUE) {
+		if (dp[K] == 100001) {
 			System.out.println(-1);
-		}else {
+		} else {
 			System.out.println(dp[K]);
 		}
 	}
