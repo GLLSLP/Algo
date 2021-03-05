bbc77d24a3d7b807184aac0ecff44d72b02db9c9
gingersnaps <37437483+dbdtrapper@users.noreply.github.com>
2021-03-06 03:36:37 +0900

    Update 백준_11653_소인수분해.java

--- 백준_11653_소인수분해.java
+++ 백준_11653_소인수분해.java
@@ -7,12 +7,12 @@ public class 백준_11653_소인수분해 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		if (N > 1) {
-			int n=2;
-			while(N!=1) {
-				if(N%n==0) {
+			int n = 2;
+			while (N != 1) {
+				if (N % n == 0) {
 					System.out.println(n);
-					N/=n;
-				}else {
+					N /= n;
+				} else {
 					n++;
 				}
 			}
