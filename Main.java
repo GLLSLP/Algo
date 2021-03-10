2bf8f64974a0a1a759b9683f0d66e2659a7d145f
gingersnaps <37437483+dbdtrapper@users.noreply.github.com>
2021-03-11 03:57:11 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class 백준_2839_설탕배달 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
