4ab88d85b86bbfdb50bc19337f7cb8f990abca43
gingersnaps <37437483+dbdtrapper@users.noreply.github.com>
2021-05-13 03:43:42 +0900

    Add files via upload

--- 백준_16500_문자열판별.java
+++ 백준_16500_문자열판별.java
@@ -3,53 +3,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class 백준_16500_문자열판별 {
-	static String line;
-	static String[] words;
-	static int len;
+	static String[] word;
+	static boolean[] dp;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		line = br.readLine();
+		String line = br.readLine();
 		int N = Integer.parseInt(br.readLine());
-		words = new String[N];
+		dp = new boolean[line.length() + 1];
+		word = new String[N];
 		for (int i = 0; i < N; i++) {
-			words[i] = br.readLine();
-			len += words[i].length();
+			word[i] = br.readLine();
 		}
-		if (dp(0)) {
+
+		dp[0] = true;
+		for (int i = 0; i < line.length(); i++) {
+			int len = i + 1;
+			for (int n = 0; n < N; n++) {
+				if (len >= word[n].length()) {
+					if (line.substring(len - word[n].length(), len).equals(word[n])) {
+						if (dp[len - word[n].length()]) {
+							dp[len] = true;
+							break;
+						}
+					}
+				}
+			}
+		}
+		if (dp[line.length()]) {
 			System.out.println(1);
 		} else {
 			System.out.println(0);
 		}
 	}
-
-	private static boolean dp(int start) {
-		// TODO Auto-generated method stub
-		if (start == line.length()) {
-			return true;
-		}
-
-		for (int i = 0; i < words.length; i++) {
-			boolean check = false;
-			String word = words[i];
-
-			if (word.length() + start > line.length()) {
-				continue;
-			}
-			for (int j = 0; j < word.length(); j++) {
-				if (line.charAt(start + j) != word.charAt(j)) {
-					check = true;
-					break;
-				}
-			}
-			if (!check) {
-				if (dp(start + word.length())) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
 }
