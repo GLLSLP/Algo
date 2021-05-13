cc7e70a005d9c5278b3a9bba2f2d38bec73d6d14
gingersnaps <37437483+dbdtrapper@users.noreply.github.com>
2021-05-14 03:30:30 +0900

    Add files via upload

---
+++ 백준_1788_피보나치수의확장.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 백준_1788_피보나치수의확장 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		if (N == 0) {
+			System.out.println(0);
+			System.out.println(0);
+		} else if (N == 1) {
+			System.out.println(1);
+			System.out.println(1);
+		} else {
+			int num = Math.abs(N);
+			int[] dp = new int[num + 1];
+			dp[0] = 0;
+			dp[1] = 1;
+			for (int i = 2; i <= num; i++) {
+				dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000000;
+			}
+			if (N < 0 && num % 2 == 0) {
+				System.out.println(-1);
+			} else {
+				System.out.println(1);
+			}
+			System.out.println(dp[num]);
+		}
+
+	}
+
+}
