4570b7ec617ba9558e1224d563ffb07ce2478151
gingersnaps <37437483+dbdtrapper@users.noreply.github.com>
2021-05-15 03:52:11 +0900

    Update 백준_9656_돌게임2.java

--- 백준_9656_돌게임2.java
+++ 백준_9656_돌게임2.java
@@ -2,37 +2,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class 백준_9656_돌게임2 {
+public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		int N=Integer.parseInt(br.readLine());
-		boolean []dp = new boolean[N+1];
-		//true가 상근이가 지는거
-		dp[0]=false;
-		dp[1]=true;
-		for (int i = 1; i <= N; i++) {
-			//승부내기
-			if(dp[i-1]==false) {
-				dp[i]=true;
-			}else {
-				dp[i]=false;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
+		dp[0] = 2;
+		dp[1] = 1;
+		for (int i = 2; i <= N; i++) {
+			if (dp[i - 1] == 1) {
+				dp[i] = 2;
+			} else {
+				dp[i] = 1;
 			}
-			
-			if(i-3<0) {
+			if (i - 3 < 0) {
 				continue;
 			}
-			if(dp[i-3]==false) {
-				dp[i]=true;
-			}else {
-				dp[i]=false;
+			if (dp[i - 3] == 1) {
+				dp[i] = 2;
+			} else {
+				dp[i] = 1;
 			}
-			
 		}
-		if(dp[N]==true) {
+		if (dp[N] == 2) {
 			System.out.println("SK");
-		}else {
+		} else if (dp[N] == 1) {
 			System.out.println("CY");
 		}
 	}
