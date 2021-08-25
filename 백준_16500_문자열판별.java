ea88becda075687ff589324011dd548f07e3b53f
SHDORI <37437483+GLLSLP@users.noreply.github.com>
2021-08-26 05:59:35 +0900

    Update 백준_16500_문자열판별.java

--- 백준_16500_문자열판별.java
+++ 백준_16500_문자열판별.java
@@ -1,40 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-public class 백준_16500_문자열판별 {
-	static String[] word;
-	static boolean[] dp;
+public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String line = br.readLine();
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String S = br.readLine();
 		int N = Integer.parseInt(br.readLine());
-		dp = new boolean[line.length() + 1];
-		word = new String[N];
-		for (int i = 0; i < N; i++) {
-			word[i] = br.readLine();
+		String[] word = new String[N];
+		for (int n = 0; n < N; n++) {
+			String line = br.readLine();
+			word[n] = line;
 		}
 
-		dp[0] = true;
-		for (int i = 0; i < line.length(); i++) {
-			int len = i + 1;
+		int[] dp = new int[S.length() + 1];
+		dp[0] = 1;
+		for (int i = 0; i < S.length(); i++) {
+			if (dp[i] == 0) {
+				continue;
+			}
 			for (int n = 0; n < N; n++) {
-				if (len >= word[n].length()) {
-					if (line.substring(len - word[n].length(), len).equals(word[n])) {
-						if (dp[len - word[n].length()]) {
-							dp[len] = true;
-							break;
-						}
-					}
+				int wordLen = word[n].length();
+				int end = i + wordLen;
+				if (end > S.length()) {
+					continue;
+				}
+				String parts = S.substring(i, end);
+				if (parts.equals(word[n])) {
+					dp[end] = 1;
+
 				}
 			}
 		}
-		if (dp[line.length()]) {
-			System.out.println(1);
-		} else {
-			System.out.println(0);
-		}
+
+		bw.write(Integer.toString(dp[S.length()]));
+		bw.flush();
+		bw.close();
 	}
 }
